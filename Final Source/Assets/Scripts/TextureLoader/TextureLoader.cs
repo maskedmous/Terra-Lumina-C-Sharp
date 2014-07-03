3d7ee819b73cc787ba77c324a94d7c97cccddebf
Kevin Mettes <k.mettes@home.nl>
2014-07-03 15:29:26 +0200

    half changes to texture loader

--- Final Source/Assets/Scripts/TextureLoader/TextureLoader.cs
+++ Final Source/Assets/Scripts/TextureLoader/TextureLoader.cs
@@ -1,4 +1,6 @@
-#pragma strict
+using UnityEngine;
+using System.Collections;
+using System.IO;
 
 ///////////////////////////////////////////////////
 //
@@ -13,42 +15,42 @@
 //
 //
 ///////////////////////////////////////////////////
-import System.IO;	//needed for searching files
 
-public class TextureLoader extends MonoBehaviour
+
+
+public class TextureLoader : MonoBehaviour
 {
 	//settings
-	public var filePath:String 			= "/FolderName";	//filePath of the textures
-	public var fileType:String 			= "*.png";			//* means all names possible  .png means files with the png extention only
-	public var nextScene:String			= "someScene";		//next scene
-	public var loadingScreen:Texture2D 	= null;				//texture2D loadingscreen
-	public var loadedLabelWidth:int		= 100;				//width and height for the % loaded
-	public var loadedLabelHeight:int	= 100;
-	public var fontSize:int				= 18;				//font size for the loading %
+	public string filePath 			= "/FolderName";	//filePath of the textures
+	public string fileType 			= "*.png";			//* means all names possible  .png means files with the png extention only
+	public string nextScene			= "someScene";		//next scene
+	public Texture2D loadingScreen 	= null;				//texture2D loadingscreen
+	public int loadedLabelWidth		= 100;				//width and height for the % loaded
+	public int loadedLabelHeight	= 100;
+	public int fontSize				= 18;				//font size for the loading %
 	
 	//private variables not to be touched
-	private var fileCount:int			= 0;				//the amount of files that are to be loaded
-	private var loadedFiles:int			= 0;				//the amount that is loaded at this point
-	private var textureArray:Array 		= new Array();		//array where we put all textures in
-	private var guiStyle:GUIStyle		= new GUIStyle();	//style for the loading % in a different font
-	private var loaded:boolean			= false;			//boolean to see if it is loaded or not
+	private int fileCount			= 0;				//the amount of files that are to be loaded
+	private int loadedFiles			= 0;				//the amount that is loaded at this point
+	private ArrayList textureArray  = new ArrayList();		//array where we put all textures in
+	private GUIStyle guiStyle		= new GUIStyle();	//style for the loading % in a different font
+	private bool  loaded			= false;			//bool  to see if it is loaded or not
 	
-	private var dotTimer:float = 1.0f;						//timer for going to the new dot
-	public var dotTexture:Texture2D = null;					//texture for the dot
-	public var dotX:float = 0.0f;							//position of the initial dot
-	public var dotY:float = 0.0f;
-	private var firstDot:boolean = true;					//booleans to show which dot
-	private var secondDot:boolean = false;
-	private var thirdDot:boolean = false;
+	private float dotTimer = 1.0f;						//timer for going to the new dot
+	public Texture2D dotTexture = null;					//texture for the dot
+	public float dotX = 0.0f;							//position of the initial dot
+	public float dotY = 0.0f;
+	private bool  firstDot = true;					//booleans to show which dot
+	private bool  secondDot = false;
+	private bool  thirdDot = false;
 	
-	private var scale:Vector2 = new Vector2();				//scale for texture scaling 16:9
-	private var originalWidth:float = 1920;					//original width and height of a 16:9 ratio
-	private var originalHeight:float = 1080;
+	private Vector2 scale = new Vector2();				//scale for texture scaling 16:9
+	private float originalWidth = 1920.0f;					//original width and height of a 16:9 ratio
+	private float originalHeight = 1080.0f;
 	//static variables not to be touched
-	static var LoaderExists:boolean = false;				//static to check if the loader exists already or not
+	static bool  LoaderExists = false;				//static to check if the loader exists already or not
 	
-	public function Awake():void
-	{
+	public void Awake (){
 		//we don't want the texture loader to be destroyed
 		DontDestroyOnLoad(this.gameObject);
 		
@@ -57,11 +59,16 @@ public class TextureLoader extends MonoBehaviour
 		{
 			LoaderExists = true;				//putting the loader exists on true
 			
-			guiStyle.font = Resources.Load("Fonts/sofachrome rg", Font);		//loading the font
-			guiStyle.normal.textColor.b = 197.0 / 255.0;						//setting the color of the font
-			guiStyle.normal.textColor.g = 185.0 / 255.0;
-			guiStyle.normal.textColor.r = 147.0 / 255.0;
-			guiStyle.fontSize = fontSize;										//setting the size of the font
+			guiStyle.font = (Font) Resources.Load("Fonts/sofachrome rg", typeof(Font));		//loading the font
+
+			Color textColor = guiStyle.normal.textColor;
+
+			textColor.b = 197.0f / 255.0f;						//setting the color of the font
+			textColor.g = 185.0f / 255.0f;
+			textColor.r = 147.0f / 255.0f;
+			guiStyle.normal.textColor = textColor;
+
+			guiStyle.fontSize = fontSize;	//setting the size of the font
 			
 			//the filePath of the textures you want to load(use the root folder, it searches through all subfolders)
 			filePath = Application.dataPath + filePath;
@@ -85,8 +92,7 @@ public class TextureLoader extends MonoBehaviour
 	}
 	
 	//checking the dot which dot to show first second or third
-	private function checkDot():void
-	{
+	private void checkDot (){
 		dotTimer -= Time.deltaTime;
 		
 		if(dotTimer <= 0.0f)
@@ -114,8 +120,7 @@ public class TextureLoader extends MonoBehaviour
 		}
 	}
 	
-	public function OnGUI():void
-	{
+	public void OnGUI (){
 		if(!loaded)
 		{
 			//scale every OnGUI call so there won't be isues resizing the window in a 16:9 aspect ratio
@@ -151,22 +156,21 @@ public class TextureLoader extends MonoBehaviour
 	}
 	
 	//Fills the arrays with the textures
-	private function fillTextureArray():IEnumerator
-	{
+	private IEnumerator fillTextureArray (){
 		//getting all files at the desired filePath with the desired fileType and searching through all directories
-		var fileInfo = Directory.GetFiles(filePath, fileType, SearchOption.AllDirectories);
+		string[] fileInfo= Directory.GetFiles(filePath, fileType, SearchOption.AllDirectories);
 		
 		fileCount = fileInfo.Length;
 		
-		for(file in fileInfo)
+		foreach(string file in fileInfo)
 		{	
 			//download the file via WWW
-			var wwwTexture = new WWW("file://"+file);
+			FIXME_VAR_TYPE wwwTexture= new WWW("file://"+file);
 			//wait for it to download
-			yield wwwTexture;
-			var texture:Texture2D = wwwTexture.texture;
+			yield return wwwTexture;
+			Texture2D texture = wwwTexture.texture;
 			//assign the name to the texture
-			var pngName:String = Path.GetFileNameWithoutExtension(file);
+			string pngName = Path.GetFileNameWithoutExtension(file);
 			texture.name = pngName;
 			//put the texture into the array
 			textureArray.push(texture);
@@ -178,10 +182,9 @@ public class TextureLoader extends MonoBehaviour
 		Application.LoadLevel(nextScene);	//its done loading so load the next scene
 	}
 	
-	//returns boolean if it is still loading files
-	public function isLoading():boolean
-	{
-		if(countedFiles() == currentLoaded())
+	//returns bool  if it is still loading files
+	public bool isLoading (){
+		 if(countedFiles() == currentLoaded())
 		{
 			return false;
 		}
@@ -190,45 +193,42 @@ public class TextureLoader extends MonoBehaviour
 	}
 	
 	//calculating the progress of loading files
-	public function percentLoaded():String
-	{
-		var part:float = currentLoaded();
-		var total:float = countedFiles();
-		var percentage:float = (part / total) * 100.0f;
+	public string percentLoaded (){
+		float part = currentLoaded();
+		float total = countedFiles();
+		float percentage = (part / total) * 100.0f;
 		percentage = Mathf.Round(percentage * 100.0f) / 100.0f;
 		return percentage.ToString();
 	}
 
 	//returns the amount of files that are to be loaded
-	public function countedFiles():int
-	{
+	public int countedFiles (){
 		return fileCount;
 	}
 	
 	//returns the amount of files that are loaded currently
-	public function currentLoaded():int
-	{
+	public int currentLoaded (){
 		return loadedFiles;
 	}
 	
 	//load additional textures into the array if the path is different
-	public function loadTextureInArray(aFilePath:String, textureName:String):IEnumerator
+	public IEnumerator loadTextureInArray ( string aFilePath ,   string textureName  )
 	{
 		//searching for the specific texture
-		var fileInfo = Directory.GetFiles(aFilePath, textureName + fileType, SearchOption.AllDirectories);
+		string[] fileInfo= Directory.GetFiles(aFilePath, textureName + fileType, SearchOption.AllDirectories);
 		//if the texture is found then the length would be 1 if not found it would be 0
 		//so if it is not 0 continue
 		if(fileInfo.Length != 0)
 		{
-			for(file in fileInfo)
+			foreach(string file in fileInfo)
 			{
 				//if the texture exists already in the array then you have doubles, conflicting
 				if(textureExistsAlready(textureName) == false)
 				{
 					//download the texture
-					var wwwTexture = new WWW("file://"+file);
+					FIXME_VAR_TYPE wwwTexture= new WWW("file://"+file);
 					//add it to the array
-					var texture:Texture2D = wwwTexture.texture;
+					Texture2D texture = wwwTexture.texture;
 					texture.name = textureName;
 					textureArray.push(texture);
 				}
@@ -246,12 +246,12 @@ public class TextureLoader extends MonoBehaviour
 	}
 	
 	//getter for textures
-	public function getTexture(textureName:String):Texture2D
+	public Texture2D getTexture ( string textureName  )
 	{		
 		//go through the textureNameArray with the given textureName
-		for(var i:int = 0; i<textureArray.length; ++i)
+		for(int i = 0; i<textureArray.length; ++i)
 		{
-			var checkTexture:Texture2D = textureArray[i] as Texture2D;
+			Texture2D checkTexture = textureArray[i] as Texture2D;
 			//if the names match
 			if(checkTexture.name == textureName)
 			{
@@ -265,12 +265,12 @@ public class TextureLoader extends MonoBehaviour
 	}
 	
 	//checks if the texture is already in the textureArray
-	private function textureExistsAlready(textureName:String):boolean
+	private bool textureExistsAlready ( string textureName  )
 	{
-		for(var i:int = 0; i<textureArray.length; ++i)
+		 for(int i = 0; i<textureArray.length; ++i)
 		{
 			//get the name from the textureNameArray and compare it with the incoming textureName
-			var texture:Texture2D = textureArray[i] as Texture2D;
+			Texture2D texture = textureArray[i] as Texture2D;
 			//if this is true then it is already in the array
 			if(texture.name == textureName)
 			{
